745ad154cae6830e5b6eb313adeeb77d55128595
jungwoonlim <wypo23@naver.com>
2019-06-22 15:21:28 +0900

    Changing the icons and testing on iOS

--- Weather.js
+++ Weather.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View, StatusBar } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-import { Ionicons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import PropTypes from "prop-types";
 
 const weatherCases = {
@@ -9,43 +9,49 @@ const weatherCases = {
     colors: ["#00C6FB", "#005BEA"],
     title: "Raining like a MF",
     subtitle: "For more info look outside",
-    icon: "ios-rainy"
+    icon: "weather-rainy"
   },
   Clear: {
     colors: ["#FEF253", "#FF7300"],
     title: "Sunnny as F",
     subtitle: "go get your ass burnt",
-    icon: "ios-sunny"
+    icon: "weather-sunny"
   },
   Thunderstorm: {
     colors: ["#00ECBC", "#007ADF"],
     title: "Thunderstorm in the hous",
     subtitle: "Actually, outside of the house",
-    icon: "ios-thunderstorm"
+    icon: "weather-lightning"
   },
   Clouds: {
     colors: ["#D7D2CC", "#304352"],
     title: "Clouds",
     subtitle: "I know, fucking boring",
-    icon: "ios-cloudy"
+    icon: "weather-cloudy"
   },
   Snow: {
     colors: ["#7DE2FC", "#B9B6E5"],
     title: "Cold as balls",
     subtitle: "Do you want to build a snowman?",
-    icon: "ios-snow"
+    icon: "weather-snowy"
   },
   Drizzle: {
     colors: ["#89F7FE", "#66A6FF"],
     title: "Drizzle",
     subtitle: "Is like rain, but not",
-    icon: "ios-rainy-outline"
+    icon: "weather-hail"
   },
   Haze: {
     colors: ["#89F7FE", "#66A6FF"],
     title: "Haze",
     subtitle: "Is like rain, but not",
-    icon: "ios-rainy-outline"
+    icon: "weather-windy"
+  },
+  Mist: {
+    colors: ["#89F7FE", "#66A6FF"],
+    title: "Haze",
+    subtitle: "Is like rain, but not",
+    icon: "weather-fog"
   }
 };
 
@@ -57,7 +63,7 @@ function Weather({ weatherName, temp }) {
     >
       <View style={styles.upper}>
         <StatusBar hidden={true} />
-        <Ionicons
+        <MaterialCommunityIcons
           color="white"
           size={144}
           name={weatherCases[weatherName].icon}
